982be3b61413300298ff7db84bad29df9d7dc822
Stephen Maher <steve@m4her.com>
2018-07-11 00:00:23 +0100

    testing of vagrant

--- provisioning.sh
+++ provisioning.sh
@@ -21,6 +21,14 @@ case $( uname -n ) in
 			yum -y update
 			rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 			yum -y install puppetserver
+			yum -y install git
+
+			# OpenSCAP scans for software issues
+			# https://www.open-scap.org/getting-started/
+			yum -y install openscap-scanner
+			yum -y install scap-workbench
+			yum -y install scap-security-guide
+
 	
 			# PE requirements for the WS !?
 			#yum install -y httpd httpd-devel mod_ssl ruby-devel rubygems gcc-c++ curl-devel zlib-devel make automake  openssl-devel
@@ -29,23 +37,53 @@ case $( uname -n ) in
 			# ( puppet-line site.pp )
 				
 
+			git https://github.com/dev-sec/puppet-os-hardening
+
 			# Modify this if you'd like to change the memory allocation, enable JMX, etc
-			JAVA_ARGS="-Xms2g -Xmx2g " #-XX:MaxPermSize=256m"
+			# JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"
+
 			sed -i "s/-XX:MaxPermSize=256m//" /etc/sysconfig/puppetserver 
 			sed -i "s/-Xms2g -Xmx2g/-Xms1g -Xmx1g/" /etc/sysconfig/puppetserver 
 
-			systemctl start puppetserver 
-			journalctl -xe 
+			systemctl start puppetserver \
+			|| journalctl -xe 
 		fi
 
 		true
 
-		puppet cert list | grep ubuntu && puppet cert sign ubuntu-agent
-		puppet cert list | grep centos && puppet cert sign centos-agent
+		# Can background this !? 
+		#puppet cert list | grep ubuntu && puppet cert sign ubuntu-agent
+		#puppet cert list | grep centos && puppet cert sign centos-agent
+		#puppet cert sign --all
+		
+		if [[ ! -s /etc/puppetlabs/puppet/autosign.conf ]]
+		then
+			echo '*' >>/etc/puppetlabs/puppet/autosign.conf
+		fi
 
-		cd / ; tar   --no-same-owner -xvf /vagrant/puppet.tar
+		# https://puppet.com/docs/pe/2017.3/code_management/r10k.html
+		# (get from r10k.yaml )
+
+		cat <<-EOF >/etc/puppetlabs/puppet/r10k.yaml
+		sources:
+		  operations:
+		   remote: 'git@github.com:mahespth/environmments
+		   basedir: '/etc/puppetlabs/code/environments'
+		   prefix: false
+		EOF
+
+		# unpack our base config
+		cd / ; tar --no-same-owner -xvf /vagrant/puppet.tar
+
+		# unpack our security hardening
 
 		puppet module list
+
+		puppet parser validate manifests/nodes.pp
+
+		
+		( cd /etc/puppetlabs/code/environments/production/modules; 
+			puppet module generate --skip-interview mahespth-lir )
 		;;
 	*)
 		if ! grep -q puppetmaster /etc/hosts
@@ -78,12 +116,8 @@ case $( uname -n ) in
 		fi
 
 		echo 'testing puppet agent"'
-		puppet agent --test --server puppetmaster --noop
+		puppet agent --test --server puppetmaster 
 		#puppet agent -t -d
-
-
-		echo "unpacking puppet.tar"
-		tar --no-same-owner -xvf puppet.tar
 
 		true
 	;;
