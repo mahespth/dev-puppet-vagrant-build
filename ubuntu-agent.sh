4ef972b1481ab8d04450b0225f7bac24d2a1c3ad
Stephen Maher <steve@m4her.com>
2018-07-11 00:00:43 +0100

    testing of vagrant

--- ubuntu-agent.sh
+++ ubuntu-agent.sh
@@ -5,6 +5,7 @@ echo "192.168.101.210 puppetmaster puppet" >>/etc/hosts
 cd /tmp
 
 wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb 
 
 apt-get -y update
